b5917a8243dbf45ca7480eda45ecbc6f24429592
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2023-02-20 19:29:11 +0100

    genmanager: corrected the KittyGen activation
    Also changed the order of arguments in __init__ now image is first, as
    it should be.

--- gen/genmanager.py
+++ gen/genmanager.py
@@ -1,17 +1,21 @@
-from gen.parsers.kitty import KittyParser
+from pathlib import Path
+from colour.extract import Extractor
+from gen.parsers.kitty import KittyGen
 
 class GenerationManager:
 
     # holds all possible configs
     __cfgs = ['kitty']
 
-    def __init__(self, configs, palette) -> None:
-        self.palette = palette
+    def __init__(self, image : Path, configs) -> None:
+        self.__image = image
+        self.palette = Extractor(self.__image).extract()
+        self.colorscheme = self.__image.stem
+
         if not isinstance(configs, bool):
             self.__cfgs = configs
 
-    """Generates specified configs."""
     def generate(self):
         """Generate colour configs for specified utilities."""
         if 'kitty' in self.__cfgs:
-            cfg = KittyParser(self.palette)
+            KittyGen(self.palette, self.colorscheme).write()
