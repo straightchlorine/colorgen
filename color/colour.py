f3fa8d2a711bce8b61802ccc92c7e6382c8faec1
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-02-08 15:54:49 +0100

    color: refactored the Colour object

--- color/colour.py
+++ color/colour.py
@@ -1,9 +1,6 @@
 # Author: Piotr Krzysztof Lis - github.com/straightchlorine
 
-"""Basic object storing all required information about given colour.
-
-Used mainly to comfortably store information about generated colors.
-"""
+"""Basic object storing all required information about given colour."""
 
 class Colour:
     """Store color in RGB and HEX format."""
@@ -12,35 +9,58 @@ class Colour:
     id : str
 
     """RGB representation of the colour."""
-    rgb : tuple
+    __rgb : tuple
 
     """Hexadecimal representation of the colour."""
-    hex : str
+    __hex : str
+
+    # rgb getters
+    @property
+    def rgb(self):
+        return self.__rgb
+
+    @rgb.getter
+    def rgb(self):
+        return self.__rgb
+
+    @rgb.setter
+    def rgb(self, rgb):
+        self.__rgb = rgb
+
+    # hex getters
+    @property
+    def hex(self):
+        return self.__hex
+
+    @hex.getter
+    def hex(self):
+        return self.__hex
+
+    @hex.setter
+    def hex(self, hex):
+        self.__hex = hex
+
+    def __rgb_to_hex(self, rgb):
+        """Convert RGB tuple to HEX string."""
+        return '#%02x%02x%02x' % rgb
 
     def __init__(self, id, rgb):
         self.id = id
-        self.rgb = rgb
-        self.hex = self.__rgb_to_hex()
-
-    def __rgb_to_hex(self):
-        """Convert RGB tuple to HEX string."""
-        return '#%02x%02x%02x' % self.rgb
+        self.__rgb = rgb
+        self.__hex = self.__rgb_to_hex(rgb)
 
     def __coloured_output(self):
         """Generate coloured output.
 
         Return four spaces with background set to stored colour.
-
         """
-        color_code = f'\033[48;2;{self.rgb[0]};{self.rgb[1]};{self.rgb[2]}m'
+        color_code = f'\033[48;2;{self.__rgb[0]};{self.__rgb[1]};{self.__rgb[2]}m'
         reset_code = '\033[0m'
         return f'{color_code}    {reset_code}'
 
     def display(self):
         """Display information about stored colour.
 
-        Output format:
         <id> <coloured_output> <hex>
-
         """
-        print('{:<15}{:^10}{:>9}'.format(self.id, self.__coloured_output(), self.hex))
+        print('{:<15}{:^10}{:>9}'.format(self.id, self.__coloured_output(), self.__hex))
