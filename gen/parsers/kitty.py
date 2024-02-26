5eb46ed7b41fdc4ae5bebb5cdb5c7d5224914ade
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-02-26 15:41:14 +0100

    kitty: cleaned up the class

--- gen/parsers/kitty.py
+++ gen/parsers/kitty.py
@@ -5,11 +5,7 @@ from pathlib import Path
 from colour.colour import Colour
 
 class KittyGen(ConfigGen):
-
-    def __check_directory(self):
-        """Check if the directory exists, if not create it."""
-        if not self.config_path.exists():
-            self.config_path.mkdir(parents=True, exist_ok=True)
+    """Generates colorcheme for kitty terminal."""
 
     def __init__(self, palette : list[Colour], colorscheme : str) -> None:
         """Initializes required resources.
@@ -19,19 +15,21 @@ class KittyGen(ConfigGen):
         super().__init__(palette, colorscheme)
         self.config_path = Path.joinpath(
                 Path.home(), '.config', 'kitty', 'colors')
-        self.filename = 'colors-' + str(colorscheme) + '.conf'
+        self.filename = str(colorscheme) + '.conf'
         self.filepath = Path.joinpath(self.config_path, self.filename)
-        self.__check_directory()
+        self.check_directory()
 
     def __write_config(self):
         """Write generated palette into kitty config file."""
         with open(self.filepath, 'w') as kitty_colors:
             for colour in self.palette:
-                kitty_colors.write('{:<12}{:<12}'.format(colour.id, colour.hex) + '\n')
+                kitty_colors.write('{:<12}{:<12}'.format(colour.id, 
+                                                         colour.hex) + '\n')
 
     def write(self):
         """Write generated palette into kitty config file.
         
-        The file is saved in $HOME/.config/kitty/colors/colors-kitty-<image_name>.conf.
+        The file is saved in:
+        $HOME/.config/kitty/colors/colors-kitty-<image_name>.conf.
         """
         self.__write_config()
