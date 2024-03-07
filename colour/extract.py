8e22b1633df2baff1f375209afd4c8bb02095329
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-03-07 18:35:26 +0100

    extractor: fixed and documented Extractor class

--- colour/extract.py
+++ colour/extract.py
@@ -3,20 +3,28 @@
 
 """Color extractor, utilises Pylette."""
 
-from Pylette import extract_colors
 from pathlib import Path
+from Pylette import extract_colors
 
 from colour.colour import Colour
 
 class Extractor:
     """
-        Extract 18 colors from the image.
+    Extractor class to generate a color scheme based on the provided image and theme.
+
+    Attributes:
+        image (Path): Path to the image.
+        theme (str): Theme type ('light' or 'dark').
+
+    Methods:
+        extract(): Extract 18 most common colors from the image.
+        colorscheme(palette): Generate a color scheme based on the provided palette.
     """
 
     """ Path to the image. """
     image : Path
 
-    colour_ids = [
+    COLOUR_ID = [
             'background',
             'foreground',
             'cursor',
@@ -38,21 +46,60 @@ class Extractor:
             'color15'
             ]
 
-    def __init__(self, image : Path) -> None:
-        self.image = image
+    def __init__(self, image : Path, theme : str) -> None:
+       """
+       Initialize the Extractor instance.
 
-    def get_colours(self, colours : list[Colour], palette) -> None:
-        i = 0
-        for c in palette:
-            rgb = (c.rgb[0], c.rgb[1], c.rgb[2])
-            colours.append(Colour(self.colour_ids[i], rgb))
-            i += 1
+       Args:
+           image (Path): Path to the image.
+           theme (str): Theme type ('light' or 'dark').
+       """
+       self.image = image
+       self.theme = theme
+
+    def __transform_to_colour_objects(self, palette) -> list[Colour]:
+        """
+        Transform given palette to a list of Colour objects.
+
+        Args:
+            palette (Palette): The input color palette.
+
+        Returns:
+            list[Colour]: A list of Colour objects i.e. generated color scheme.
+        """
+        colours = []
+
+        # background
+        colours.append(Colour(self.COLOUR_ID[0],
+                              palette[0].rgb))
+        # foreground
+        colours.append(Colour(self.COLOUR_ID[1],
+                              palette[len(palette) - 1].rgb))
+        # cursor
+        colours.append(Colour(self.COLOUR_ID[2],
+                              palette[len(palette) - 2].rgb))
+
+        for i, index in enumerate(range(len(palette) - 3, 1, -1), start=4):
+            colours.append(Colour(self.COLOUR_ID[i], palette[index].rgb))
+
+        return colours
 
     def extract(self):
         """
-            Extract 18 most common colors from the image.
+        Extract 18 most common colors from the image.
+
+        Returns:
+            list[Colour]: A list of Colour objects i.e. generated color scheme.
         """
-        palette = extract_colors(self.image, palette_size=18, resize = True, sort_mode = 'luminance')
-        colours = []
-        self.get_colours(colours, palette)
-        return colours
+
+        palette = extract_colors(self.image,
+                                 palette_size=19,
+                                 resize = True,
+                                 sort_mode = 'luminance')
+
+        # the palette is sorted by luminance, thus simply reversing it will
+        # provide lighter colors
+        if self.theme == 'light':
+            palette = palette[::-1]
+
+        return self.__transform_to_colour_objects(palette)
