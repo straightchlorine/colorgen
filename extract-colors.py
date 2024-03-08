b2de2c9d73f9d9e45d4b6af44e2bed3e90c4305f
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-03-08 19:42:16 +0100

    main: modified the parser and added documentation

--- extract-colors.py
+++ extract-colors.py
@@ -5,27 +5,59 @@ from pathlib import Path
 
 from gen.genmanager import GenerationManager
 
-"""Parses the arguments passed by the user."""
 class ArgumentParser:
+    """
+    ArgumentParser class for parsing command-line arguments.
+
+    Attributes:
+        parser (argparse.ArgumentParser): The argument parser instance.
+        args (argparse.Namespace): Parsed command-line arguments.
+
+    Methods:
+        __init__(): Initializes the ArgumentParser instance.
+    """
     @property
     def parser(self):
-        return self.__parser
+        """
+        Get the argument parser instance.
 
-    @parser.getter
-    def parser(self):
+        Returns:
+            argparse.ArgumentParser: The argument parser instance.
+        """
         return self.__parser
 
     @property
     def args(self):
+        """
+        Get the parsed command-line arguments.
+
+        Returns:
+            argparse.Namespace: Parsed command-line arguments.
+        """
         return self.__parser.parse_args()
 
     def __init__(self) -> None:
+        """
+        Initialize the ArgumentParser instance with command-line argument definitions.
+        """
+
         self.__parser = argparse.ArgumentParser(
             description='Creates a colorscheme based on an image and creates \
                 an colour files for given utilities.',
             epilog='Author: Piotr Krzysztof Lis - github.com/straightchlorine')
 
-        # add mutuall exclusive group for config and full config
+        group = self.parser.add_mutually_exclusive_group()
+        group.add_argument('--config', '-c',
+                            help='Generate config for a given utilities',
+                            type=str,
+                            choices=['kitty', 'awesome', 'rofi'],
+                            action='extend', nargs='+')
+
+        group.add_argument('--full-config', '-fc',
+                            help='Generatre config for every single \
+                            offered utility',
+                            action='store_true')
+
         self.parser.add_argument('image',
                                  help='Path to the image file',
                                  type=lambda p: Path(p).absolute())
@@ -43,27 +75,26 @@ class ArgumentParser:
                                  help='Replace existing colorscheme',
                                  action='store_true')
 
-        self.parser.add_argument('--config', '-c',
-                                 help='Generate config for a given utilities',
-                                 type=str,
-                                 choices=['kitty', 'awesome', 'rofi'],
-                                 action='extend', nargs='+')
-
-        self.parser.add_argument('--full-config', '-fc',
-                                 help='Generatre config for every single \
-                                    offered utility', 
-                                 action='store_true')
-
-"""Main module for the script"""
 class ExtractColors:
+    """
+    Main module for the script.
+
+    Methods:
+        __init__(): Initializes the ExtractColors instance.
+    """
 
     def __init__(self):
+        """
+        Initialize the ExtractColors instance, parse command-line arguments,
+        and generate color configurations.
+        """
         parser = ArgumentParser()
         args = parser.args
 
-        cfg = args.config if args.config else args.full_config
-        configs = GenerationManager(args.image, cfg)
-
+        configs = GenerationManager(args.image,
+                                    args.config if
+                                        args.config else args.full_config,
+                                    args.theme)
         configs.generate()
 
 if __name__ == "__main__":
