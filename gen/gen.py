8fb9bdae81c57f1cad1654ca408b1704716cb76e
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-03-12 15:11:28 +0100

    gen: updated gen class to also have apply method

--- gen/gen.py
+++ gen/gen.py
@@ -3,9 +3,14 @@
 from colour.colour import Colour
 from pathlib import Path
 
+class ConfigNotFoundException(Exception):
+    def __init__(self, message : str):
+        super().__init__(f'{message} - configuration file not found.')
+        
 class ConfigGen:
     """Base class for all config generators."""
 
+    colors_path : Path
     config_path : Path
 
     def __init__(self, palette : list[Colour], colorscheme : str) -> None:
@@ -14,8 +19,15 @@ class ConfigGen:
 
     def check_directory(self):
         """Check if the directory exists, if not create it."""
+        if not self.colors_path.exists():
+            self.colors_path.mkdir(parents=True, exist_ok=True)
+
+    def check_config(self):
         if not self.config_path.exists():
-            self.config_path.mkdir(parents=True, exist_ok=True)
+            raise ConfigNotFoundException(str(self.config_path))
 
     def write(self):
+        pass
+
+    def apply(self):
         pass
