34ca562b653b68741a8a517ef9942c502af4abd2
Piotr Krzysztof Lis <piotrlis555@gmail.com>
2024-03-13 20:53:08 +0100

    test: testing utility for new apply function implementation

--- tests/test_gen.py
+++ tests/test_gen.py
@@ -60,3 +60,38 @@ class TestGen(unittest.TestCase):
             # print(self.gen.filepath.read_text())
             self.gen.filepath.unlink()
         self.assertTrue(flag)
+
+    def test_apply_kitty(self):
+        self.palette = Extractor(self.image, 'dark').extract()
+        self.colorscheme = self.image.stem
+
+        self.gen = KittyGen(self.palette, self.colorscheme)
+        self.gen.config_path = Path.joinpath(Path.cwd(), 
+                                'tests', 'cfg', 'kitty.conf')
+
+        # save the original config
+        with open(self.gen.config_path, 'r') as kitty_cfg:
+            backup = kitty_cfg.readlines()
+
+        self.gen.apply()
+
+        # read the changes 
+        with open(self.gen.config_path, 'r') as kitty_cfg:
+            lines = kitty_cfg.readlines()
+
+        # restore the original config
+        with open(self.gen.config_path, 'w') as kitty_cfg:
+            for line in backup:
+                kitty_cfg.write(line)
+
+        flag = False
+        times = 0
+        for line in lines:
+            if 'include colors/' + self.gen.filename in line:
+                times += 1
+
+        if times == 1:
+            flag = True
+
+        self.assertTrue(flag)
+
