863530c4e5bcc961de836e38bb6587cfe25fac85
Piotr Krzysztof <piotrlis555@gmail.com>
2025-11-15 15:32:18 +0100

    feat(docker): added and reorganised

---
+++ exceptions.py
@@ -0,0 +1,39 @@
+# Author: Piotr Krzysztof Lis - github.com/straightchlorine
+
+"""Exception hierarchy for colorgen."""
+
+
+class ColorgenException(Exception):
+    """Base exception for all colorgen errors."""
+
+    pass
+
+
+class InvalidImageError(ColorgenException):
+    """Raised when image path is invalid or image cannot be loaded."""
+
+    pass
+
+
+class InvalidColorError(ColorgenException):
+    """Raised when color values are invalid (e.g., RGB out of range)."""
+
+    pass
+
+
+class ConfigWriteError(ColorgenException):
+    """Raised when configuration file cannot be written."""
+
+    pass
+
+
+class ModelLoadError(ColorgenException):
+    """Raised when ML model cannot be loaded (for future ML integration)."""
+
+    pass
+
+
+class ValidationError(ColorgenException):
+    """Raised when input validation fails."""
+
+    pass
